548045d5e84a9454b42649b839c404d5153d19f2
Shuvael <65926308+Shuvael@users.noreply.github.com>
2026-06-26 02:25:37 +0200

    - middleware angepasst

--- quizingo-backend/main.py
+++ quizingo-backend/main.py
@@ -12,7 +12,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://quizingo.vercel.app/",  # deine echte Vercel-URL eintragen
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
